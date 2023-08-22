1f0a606e4916ca0f476d780e5ec6ffa44cef88ea
Emre ÜNALDI <emree.unaldi@gmail.com>
2023-08-22 13:04:18 +0300

    Spring Data, Spring Rest

---
+++ src/main/java/com/emreunaldi/springboot/Bean/PasswordEncoderBean.java
@@ -0,0 +1,14 @@
+package com.emreunaldi.springboot.bean;
+
+/*import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;*/
+
+//@Configuration
+public class PasswordEncoderBean {
+    /*@Bean
+    public PasswordEncoder passwordEncoderBean() {
+        return new BCryptPasswordEncoder();
+    }*/
+}
---
+++ src/main/java/com/emreunaldi/springboot/data/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.emreunaldi.springboot.data.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository {
+}
